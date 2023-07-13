ca51811a273b09a46c799c2faa27c8ee99b6681c
ameliasm11 <ameliasyamsa1309@gmail.com>
2023-07-13 11:05:37 +0700

    Katalon part 3

---
+++ Test Suites/TestSuite2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b5ded58e-9724-41f6-978b-d44edc07ec42</testSuiteGuid>
+   <testCaseLink>
+      <guid>cbae298e-82d7-4de9-8f05-9c3b0d6ea80c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestListenersDemo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
